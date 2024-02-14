972ebd32e4f43fa0752150c6adab19262d647a88
scott mcgann <s.r.mcgann@hotmail.com>
2024-02-14 20:57:25 +0000

    sync

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader');
+
+module.exports = {
+  devServer: {
+    host: '192.168.1.201',
+    allowedHosts: [
+      '192.168.1.201,127.0.0.1,local.whitehotrobot.com'
+    ],
+	  port:8000,
+  },
+  publicPath: './',
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('html').tap((opts) => {
+        opts[0].filename = './index.php';
+        return opts;
+      });
+    }
+  }
+}
